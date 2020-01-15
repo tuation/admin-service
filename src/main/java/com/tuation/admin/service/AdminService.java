48d141322af8ac7e39a9bc438403c6d1728a7d2a
Debasis Mohapatra <ds.tuation@gmail.com>
2020-01-15 10:08:32 +0530

    main and test method added

---
+++ src/main/java/com/tuation/admin/service/AdminService.java
@@ -0,0 +1,12 @@
+package com.tuation.admin.service;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminService.class, args);
+	}
+
+}
